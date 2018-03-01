85ba2fbb64a5f68d4057e367f63b181951181a02
骐骏 <xgs2168@foxmail.com>
2018-03-01 18:59:40 +0800

    druid

---
+++ druidStudy/src/main/java/com/fullstacker/course/DruidStudyApplication.java
@@ -0,0 +1,12 @@
+package com.fullstacker.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DruidStudyApplication.class, args);
+	}
+}
---
+++ druidStudy/src/test/java/com/fullstacker/course/DruidStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstacker.course;
+
+import org.junit.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class DruidStudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
